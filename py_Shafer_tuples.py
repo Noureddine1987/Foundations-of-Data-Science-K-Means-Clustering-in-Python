a29695a05cf53fa985207f1993ae78b2facbc819
noureddine1987 <boukhari.noureddine@gmail.com>
2023-08-03 11:31:06 +0200

    tuples

--- py_Shafer_tuples.py
+++ py_Shafer_tuples.py
@@ -7,7 +7,7 @@ print(courses_tuple1)
 print(courses_tuple2)
 
 # we can only to loop through and access : TypeError: 'tuple' object does not support item assignment
-courses_tuple1[0] = 'Algo'
+# courses_tuple1[0] = 'Algo'
 
 print(courses_tuple1[0])
 
